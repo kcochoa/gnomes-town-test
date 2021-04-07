0d19e0d55389e0ff494034cd2e2ded842f8b2af9
katia <kcamivo90@gmail.com>
2021-04-07 16:34:04 +0200

    *Create skeleton and mainly styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import SearchResult from "./pages/SearchResult";
+import GnomeDetails from "./pages/GnomeDetails";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+      <Switch>
+      <Route path="/all-gnomes" exact>
+        <SearchResult />
+      </Route>
+      <Route path="/gnome/:id" exact>
+        <GnomeDetails />
+      </Route>
+      <Route path="/" exact>
+        <HomePage />
+      </Route>
+    </Switch>
+
+   </Router> 
+   
   );
 }
 
