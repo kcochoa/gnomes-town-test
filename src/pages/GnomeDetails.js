8a260337219d629186b8ee9967face69a5650e48
katia <kcamivo90@gmail.com>
2021-04-07 16:33:23 +0200

    * Create main pages of the app

---
+++ src/pages/GnomeDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GnomeDetails = () => {
+    return ( <div>Gnomes Details Page</div> );
+}
+ 
+export default GnomeDetails;
---
+++ src/pages/SearchResult.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchBar from "../components/SearchBar/SearchBar"
+
+const Gnomes = () => {
+  return (
+    <SearchBar/>
+  );
+};
+
+export default Gnomes;
