4dbe00a908e00d7a427c24468ba24640aef6f32e
katia <kcamivo90@gmail.com>
2021-04-08 20:48:36 +0200

    *Add new images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import SearchResult from "./pages/SearchResult";
-import GnomeDetails from "./pages/GnomeDetails";
+import ListHabitants from "./pages/ListHabitants";
+import HabitantDetails from "./pages/HabitantDetails";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
    <Router>
       <Switch>
-      <Route path="/all-gnomes" exact>
-        <SearchResult />
+      <Route path="/all-habitants" exact>
+        <ListHabitants />
       </Route>
-      <Route path="/gnome/:id" exact>
-        <GnomeDetails />
+      <Route path="/habitant/:id" exact>
+        <HabitantDetails />
       </Route>
       <Route path="/" exact>
         <HomePage />
---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import rootReducer from "./redux/root-reducer";
+
+const middleware = [thunk];
+
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
