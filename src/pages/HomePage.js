7833d30d74835dde56aa77b2c4be1e3281d545f9
katia <kcamivo90@gmail.com>
2021-04-08 20:52:04 +0200

    *Create homepage with styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import {Link} from "react-router-dom"
 
 const HomePage = () => {
-    return ( <div>HomePage</div> );
-}
- 
+  return (
+    <Container className="bg-banner d-flex flex-column" fluid>
+      <Row className="my-auto">
+        <Col xs={6} sm={4} lg={3} xl={3} className="align-items-center mx-auto" >
+          <Button className="w-100 btn-banner" ><Link className="link-banner" to="/all-habitants">GET TO KNOW US </Link></Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
 export default HomePage;
